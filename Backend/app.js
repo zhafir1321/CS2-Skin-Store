34544361216f6927b08766591b880c5839dc73d1
Zhafir <zahfir1000@hotmail.com>
2024-09-06 11:36:53 +0700

    added

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const cors = require("cors");
 const router = require("./routers/index");
 
--- Backend/helpers/jwt.js
+++ Backend/helpers/jwt.js
@@ -1,9 +1,9 @@
-const jwt = require('jsonwebtoken');
-const secretKey = 'rahasia';
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.JWT_SECRET_KEY;
 
 const signToken = (payload) => {
   return jwt.sign(payload, secretKey, {
-    expiresIn: '1h',
+    expiresIn: "1h",
   });
 };
 
